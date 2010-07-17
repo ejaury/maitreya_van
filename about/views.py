cb301dc0fcb11514c0dde350e48241833843efca
Edwin Jaury <git@edwinjaury.com>
2010-07-16 17:23:27 -0700

    Created base layout for pages other than main page.

--- about/views.py
+++ about/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def contact_us(request):
+  return render_to_response('about/contact_us.html')
+
--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^$', 'maitreya_van.main.views.index'),
+    (r'^about/', include('maitreya_van.about.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
       'document_root': '/home/edwin/maitreya_van/assets/', 'show_indexes': True
