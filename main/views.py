b4ef5e9b415a5184c719bbd0609763aa00303da9
Edwin Jaury <git@edwinjaury.com>
2010-08-23 21:28:11 -0700

    Added upcoming event list context to main page.

--- main/views.py
+++ main/views.py
@@ -1,4 +1,12 @@
+import datetime
+from django.conf import settings
 from django.shortcuts import render_to_response
+from maitreya_van.schedule.models import Event
 
 def index(request):
-  return render_to_response('main/index.html')
+  event_limit = int(getattr(settings, 'UPCOMING_EVENTS_LIMIT', 3))
+  events = Event.objects.filter(start__gt=datetime.datetime.today()).order_by('start',
+    'title')[:event_limit]
+  return render_to_response('main/index.html', {
+      'events': events,
+    })
