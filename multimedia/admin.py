ac2a3f01cbb8eeea378b0967100703afc8fdee03
Edwin Jaury <git@edwinjaury.com>
2011-05-21 14:49:30 -0700

    Fixed indentation.

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -3,7 +3,10 @@ from maitreya_van.multimedia.models import PhotoGallery, PhotoGalleryUpload
 from photologue.admin import GalleryAdmin
 
 class PhotoGalleryAdmin(GalleryAdmin):
-  filter_horizontal = ('categories', 'photos',)
+    filter_horizontal = ('categories', 'photos',)
+
+class PhotoGalleryUploadAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(PhotoGallery, PhotoGalleryAdmin)
 admin.site.register(PhotoGalleryUpload)
--- multimedia/models.py
+++ multimedia/models.py
@@ -3,12 +3,13 @@ from photologue.models import Gallery, GalleryUpload
 from maitreya_van.general.models import Category
 
 class PhotoGallery(Gallery):
-  categories = models.ManyToManyField(Category, limit_choices_to = {
+    categories = models.ManyToManyField(Category, limit_choices_to = {
                 'content_type__model': 'PhotoGallery',
                })
 
-  class Meta:
-    verbose_name_plural = 'Photo Galleries'
+    class Meta:
+        verbose_name_plural = 'Photo Galleries'
+
 
 class PhotoGalleryUpload(GalleryUpload):
-  pass
+    pass
