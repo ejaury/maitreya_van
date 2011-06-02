ca9d85c7eca0f35b39a7e41dddd45646bae1932e
Edwin Jaury <git@edwinjaury.com>
2011-06-01 21:24:18 -0700

    Style fixes for Photo gallery page.

--- multimedia/templatetags/multimedia_tags.py
+++ multimedia/templatetags/multimedia_tags.py
@@ -7,14 +7,14 @@ register = template.Library()
 def next_photo_url(photo, gallery):
   next = photo.get_next_in_gallery(gallery)
   if next:
-      return '<a title="%s" href="%s">Next >></a>' % (next.title, next.get_absolute_url())
+      return '<a class="nav" title="%s" href="%s">Next</a>' % (next.title, next.get_absolute_url())
   return ""
 
 @register.simple_tag
 def previous_photo_url(photo, gallery):
   prev = photo.get_previous_in_gallery(gallery)
   if prev:
-      return '<a title="%s" href="%s"><< Previous</a>' % (prev.title, prev.get_absolute_url())
+      return '<a class="nav" title="%s" href="%s">Previous</a>' % (prev.title, prev.get_absolute_url())
   return ""
 
 @register.filter(name='remainder')
