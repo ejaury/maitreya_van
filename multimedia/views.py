affc4f26d51f6c6a97f54191ae6695c2ca2a06a8
Edwin Jaury <git@edwinjaury.com>
2011-06-19 00:19:59 -0700

    Implemented music playlist from jplayer demo.

--- multimedia/views.py
+++ multimedia/views.py
@@ -2,17 +2,21 @@ import os.path
 from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils import simplejson as json
+from django.utils.safestring import mark_safe
 
 from maitreya_van.multimedia.models import Music
 
 def music_gallery_view(request):
-    song_url = ''
-    if Music.objects.count() > 0:
-        music = Music.objects.all()[0]
-        song_url = os.path.join(settings.MEDIA_URL, music.file.name)
+    playlist = []
+    for music in Music.objects.all():
+        playlist.append({
+            'name': music.song_title,
+            'mp3': os.path.join(settings.MEDIA_URL, music.file.name),
+        })
     context = {
         'title': 'Music Gallery',
-        'song_url': song_url,
+        'playlist': mark_safe(json.dumps(playlist)),
     }
     return render_to_response('multimedia/music_index.html',
         context, context_instance=RequestContext(request))
