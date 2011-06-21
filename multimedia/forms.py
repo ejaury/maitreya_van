a37cb97bfbc7d03388dc54d4cb61a1baf6274d6b
Edwin Jaury <git@edwinjaury.com>
2011-06-20 22:04:37 -0700

    Validate uploaded MP3 file.

--- multimedia/forms.py
+++ multimedia/forms.py
@@ -1,6 +1,7 @@
 import re
 from django import forms
 
+from maitreya_van.multimedia import utils
 from maitreya_van.multimedia.models import Music
 
 
@@ -8,6 +9,12 @@ class MusicUploadForm(forms.ModelForm):
     class Meta:
         model = Music
         fields = ('file', 'tags')
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+        if not utils.is_mp3(file.name):
+            raise forms.ValidationError('"%s" is not an MP3 file' % file.name)
+        return file
 
     def save(self, commit=True):
         music = super(MusicUploadForm, self).save(commit=False)
---
+++ multimedia/utils.py
@@ -0,0 +1,3 @@
+def is_mp3(filename):
+    """Determine if file is an MP3"""
+    return filename.endswith('.mp3')
