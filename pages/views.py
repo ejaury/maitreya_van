7209603b29774b7eb0b17c8829c6c8a27216f67f
Edwin Jaury <git@edwinjaury.com>
2011-08-05 06:12:39 -0700

    More title fixes.

--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,29 @@
 from django.views.generic import DetailView, ListView, TemplateView
 
 
-class ContextDataMixin(object):
+class PageListView(ListView):
+    context_object_name = 'object_list'
+    template_name = 'pages/index.html'
+
     def get_context_data(self, **kwargs):
-        context = super(ContextDataMixin, self).get_context_data(**kwargs)
+        context = super(PageListView, self).get_context_data(**kwargs)
         name = self.model._meta.verbose_name_plural
         context['title'] = name.title()
         return context
 
 
-class PageListView(ContextDataMixin, ListView):
-    context_object_name = 'object_list'
-    template_name = 'pages/index.html'
-
-
-class PageDetailView(ContextDataMixin, DetailView):
+class PageDetailView(DetailView):
     context_object_name = 'object'
     template_name = 'pages/view_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PageDetailView, self).get_context_data(**kwargs)
+        obj = kwargs.get('object')
+        if obj and hasattr(obj, 'title'):
+            context['title'] = obj.title
+        else:
+            context['title'] = self.model._meta.verbose_name.title()
+        return context
 
 
 class ContactView(TemplateView):
