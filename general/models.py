822ef3f385f76890e8863fdb6762abc1946af23a
Edwin (Ubuntu PC) <git@edwinjaury.com>
2012-05-07 23:21:03 -0700

    Handle menu item deletion when a page is deleted, for both bulk delete and regular delete.

--- general/models.py
+++ general/models.py
@@ -4,6 +4,7 @@ import logging
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sitemaps import ping_google
 from django.db import models
+from django.db.models.signals import class_prepared, post_delete
 from django.utils.encoding import force_unicode
 from exceptions import NotImplementedError
 from maitreya_van.navigation.models import MenuItemExtension
@@ -11,6 +12,29 @@ from tagging.fields import TagField
 from treemenus.models import MenuItem
 
 logger = logging.getLogger(__name__)
+
+
+class BasePageManager(models.Manager):
+    def contribute_to_class(self, model, name):
+        # Setup post_delete handler via class_prepared as a workaround, since we
+        # can't attach post_delete to an abstract class BasePage
+        class_prepared.connect(setup_postdelete, sender=model)
+        super(BasePageManager, self).contribute_to_class(model, name)
+
+def page_postdelete(sender, **kwargs):
+    """A post delete handler for BasePage."""
+    page = kwargs['instance']
+    if MenuItem.objects.filter(pk=page.menu_item.pk).count() > 0:
+        try:
+            menu_ext = MenuItemExtension.objects.get(menu_item=page.menu_item)
+            menu_ext.delete()
+        except MenuItemExtension.DoesNotExist:
+            pass
+        page.menu_item.delete()
+
+def setup_postdelete(sender, **kwargs):
+    """A hook to connect post_delete handler for BasePage subclasses."""
+    post_delete.connect(page_postdelete, sender=sender)
 
 
 class BasePage(models.Model):
@@ -28,21 +52,13 @@ class BasePage(models.Model):
     updated_at = models.DateTimeField(auto_now=True,
         default=datetime.datetime.today())
 
+    objects = BasePageManager()
+
     class Meta:
         abstract = True
 
     def __unicode__(self):
         return self.title
-
-    def delete(self, *args, **kwargs):
-        if self.menu_item:
-            try:
-                menu_ext = MenuItemExtension.objects.get(menu_item=self.menu_item)
-                menu_ext.delete()
-            except MenuItemExtension.DoesNotExist:
-                pass
-            self.menu_item.delete()
-        super(BasePage, self).delete(*args, **kwargs)
 
     def save(self, *args, **kwargs):
         super(BasePage, self).save(*args, **kwargs)
