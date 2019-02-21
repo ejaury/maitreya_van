d4645c19dee05587f7c7bdf849db8cfd53c75cdd
Edwin <git@edwinjaury.com>
2019-02-21 11:34:37 -0800

    Remove regular service schedule, add public English class schedule, hide old media and news

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import os.path
 import random
 
 from django.conf import settings
@@ -21,10 +22,16 @@ def index(request):
     # Populate latest news
     news = News.objects.all()[:content_limit]
 
+    eng_class_rel_path = 'files/english-class.pdf'
+    eng_class_path = os.path.join(settings.STATIC_ROOT, eng_class_rel_path)
+    if not os.path.isfile(eng_class_path):
+        eng_class_rel_path = None
+
     return render_to_response('main/index.html', {
         'photo_urls': photo_urls,
         'news_list': news,
         'latest_media': latest_media,
+        'eng_class_rel_path': eng_class_rel_path,
     }, context_instance=RequestContext(request))
 
 def get_random_photo_urls():
